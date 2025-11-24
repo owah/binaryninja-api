5552742668565422465239583383323818806856
Glenn Smith <glenn@vector35.com>
2025-11-24 12:52:13 -0500

    DBO: Metadata api

--- binaryninjaapi.h
+++ binaryninjaapi.h
@@ -23352,6 +23352,7 @@ namespace BinaryNinja {
 		int GetType() const;
 		std::string GetId() const;
 		std::string GetDescription() const;
+		Ref<Metadata> GetMetadata() const;
 		Ref<DatabaseObject> GetParent() const;
 		std::unordered_map<std::string, Ref<DatabaseObject>> GetChildren();
 		std::vector<std::string> GetDependencies() const;
